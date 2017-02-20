898a9f7e649b8a17f9a92bd7971add3be8c6ea61
Bryan Hampton <bryan@zeropointfrontiers.com>
2017-02-20 15:10:11 -0600

    Just fixing text alignment

--- UtilLines.py
+++ UtilLines.py
@@ -14,9 +14,9 @@ class LaneLines:
         color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
 
         # Recast the x and y points into usable format for cv2.fillPoly()
-        pts_left = np.array([np.transpose(np.vstack([self.left_fitx, self.ploty]))])
-        pts_right = np.array([np.flipud(np.transpose(np.vstack([self.right_fitx, self.ploty])))])
-        pts = np.hstack((pts_left, pts_right))
+        pts_lft = np.array([np.transpose(np.vstack([self.lft_fitx, self.ploty]))])
+        pts_rgt = np.array([np.flipud(np.transpose(np.vstack([self.rgt_fitx, self.ploty])))])
+        pts = np.hstack((pts_lft, pts_rgt))
 
         # Draw the lane onto the warped blank image
         cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
@@ -31,11 +31,11 @@ class LaneLines:
         histogram = np.sum(binary_warped[binary_warped.shape[0]/2:,:], axis=0)
         # Create an output image to draw on and  visualize the result
         out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
-        # Find the peak of the left and right halves of the histogram
-        # These will be the starting point for the left and right lines
+        # Find the peak of the lft and rgt halves of the histogram
+        # These will be the starting point for the lft and rgt lines
         midpoint = np.int(histogram.shape[0]/2)
-        leftx_base = np.argmax(histogram[:midpoint])
-        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+        lftx_base = np.argmax(histogram[:midpoint])
+        rgtx_base = np.argmax(histogram[midpoint:]) + midpoint
 
         # Choose the number of sliding windows
         nwindows = 9
@@ -46,76 +46,76 @@ class LaneLines:
         nonzeroy = np.array(nonzero[0])
         nonzerox = np.array(nonzero[1])
         # Current positions to be updated for each window
-        leftx_current = leftx_base
-        rightx_current = rightx_base
+        lftx_current = lftx_base
+        rgtx_current = rgtx_base
         # Set the width of the windows +/- margin
         margin = 100
         # Set minimum number of pixels found to recenter window
         minpix = 50
-        # Create empty lists to receive left and right lane pixel indices
-        left_lane_inds = []
-        right_lane_inds = []
+        # Create empty lists to receive lft and rgt lane pixel indices
+        lft_lane_inds = []
+        rgt_lane_inds = []
 
         # Step through the windows one by one
         for window in range(nwindows):
-            # Identify window boundaries in x and y (and right and left)
-            win_y_low = binary_warped.shape[0] - (window+1)*window_height
-            win_y_high = binary_warped.shape[0] - window*window_height
-            win_xleft_low = leftx_current - margin
-            win_xleft_high = leftx_current + margin
-            win_xright_low = rightx_current - margin
-            win_xright_high = rightx_current + margin
+            # Identify window boundaries in x and y (and rgt and lft)
+            win_y_lo = binary_warped.shape[0] - (window+1)*window_height
+            win_y_hi = binary_warped.shape[0] - window*window_height
+            win_xlft_lo = lftx_current - margin
+            win_xlft_hi = lftx_current + margin
+            win_xrgt_lo = rgtx_current - margin
+            win_xrgt_hi = rgtx_current + margin
             # Draw the windows on the visualization image
-            cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
-            cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
+            cv2.rectangle(out_img,(win_xlft_lo,win_y_lo),(win_xlft_hi,win_y_hi),(0,255,0), 2) 
+            cv2.rectangle(out_img,(win_xrgt_lo,win_y_lo),(win_xrgt_hi,win_y_hi),(0,255,0), 2) 
             # Identify the nonzero pixels in x and y within the window
-            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
-            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+            good_lft_inds = ((nonzeroy >= win_y_lo) & (nonzeroy < win_y_hi) & (nonzerox >= win_xlft_lo) & (nonzerox < win_xlft_hi)).nonzero()[0]
+            good_rgt_inds = ((nonzeroy >= win_y_lo) & (nonzeroy < win_y_hi) & (nonzerox >= win_xrgt_lo) & (nonzerox < win_xrgt_hi)).nonzero()[0]
             # Append these indices to the lists
-            left_lane_inds.append(good_left_inds)
-            right_lane_inds.append(good_right_inds)
+            lft_lane_inds.append(good_lft_inds)
+            rgt_lane_inds.append(good_rgt_inds)
             # If you found > minpix pixels, recenter next window on their mean position
-            if len(good_left_inds) > minpix:
-                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
-            if len(good_right_inds) > minpix:        
-                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+            if len(good_lft_inds) > minpix:
+                lftx_current = np.int(np.mean(nonzerox[good_lft_inds]))
+            if len(good_rgt_inds) > minpix:        
+                rgtx_current = np.int(np.mean(nonzerox[good_rgt_inds]))
 
         # Concatenate the arrays of indices
-        left_lane_inds = np.concatenate(left_lane_inds)
-        right_lane_inds = np.concatenate(right_lane_inds)
+        lft_lane_inds = np.concatenate(lft_lane_inds)
+        rgt_lane_inds = np.concatenate(rgt_lane_inds)
 
-        # Extract left and right line pixel positions
-        leftx = nonzerox[left_lane_inds]
-        lefty = nonzeroy[left_lane_inds] 
-        rightx = nonzerox[right_lane_inds]
-        righty = nonzeroy[right_lane_inds] 
+        # Extract lft and rgt line pixel positions
+        lftx = nonzerox[lft_lane_inds]
+        lfty = nonzeroy[lft_lane_inds] 
+        rgtx = nonzerox[rgt_lane_inds]
+        rgty = nonzeroy[rgt_lane_inds] 
 
         # Fit a second order polynomial to each
-        left_fit = np.polyfit(lefty, leftx, 2)
-        right_fit = np.polyfit(righty, rightx, 2)
+        lft_fit = np.polyfit(lfty, lftx, 2)
+        rgt_fit = np.polyfit(rgty, rgtx, 2)
     
     
         # Generate x and y values for plotting
         self.ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
-        self.left_fitx = left_fit[0]*self.ploty**2 + left_fit[1]*self.ploty + left_fit[2]
-        self.right_fitx = right_fit[0]*self.ploty**2 + right_fit[1]*self.ploty + right_fit[2]
+        self.lft_fitx = lft_fit[0]*self.ploty**2 + lft_fit[1]*self.ploty + lft_fit[2]
+        self.rgt_fitx = rgt_fit[0]*self.ploty**2 + rgt_fit[1]*self.ploty + rgt_fit[2]
 
-        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+        out_img[nonzeroy[lft_lane_inds], nonzerox[lft_lane_inds]] = [255, 0, 0]
+        out_img[nonzeroy[rgt_lane_inds], nonzerox[rgt_lane_inds]] = [0, 0, 255]
     
         # Fit new polynomials to x,y in world space
-        left_fit_cr = np.polyfit(self.ploty*ym_per_pix, self.left_fitx*xm_per_pix, 2)
-        right_fit_cr = np.polyfit(self.ploty*ym_per_pix, self.right_fitx*xm_per_pix, 2)
+        lft_fit_cr = np.polyfit(self.ploty*ym_per_pix, self.lft_fitx*xm_per_pix, 2)
+        rgt_fit_cr = np.polyfit(self.ploty*ym_per_pix, self.rgt_fitx*xm_per_pix, 2)
         # Calculate the new radii of curvature
         y_eval = np.max(self.ploty)
-        left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
-        right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+        lft_curverad = ((1 + (2*lft_fit_cr[0]*y_eval*ym_per_pix + lft_fit_cr[1])**2)**1.5) / np.absolute(2*lft_fit_cr[0])
+        rgt_curverad = ((1 + (2*rgt_fit_cr[0]*y_eval*ym_per_pix + rgt_fit_cr[1])**2)**1.5) / np.absolute(2*rgt_fit_cr[0])
         # Now our radius of curvature is in meters
-        print(left_curverad, 'm', right_curverad, 'm')
+        print(lft_curverad, 'm', rgt_curverad, 'm')
     
         # calculate weighted average
-        curveTot = left_curverad*len(leftx) + right_curverad*len(rightx)
-        curve_radius = curveTot/(len(leftx) + len(rightx))
+        curveTot = lft_curverad*len(lftx) + rgt_curverad*len(rgtx)
+        curve_radius = curveTot/(len(lftx) + len(rgtx))
     
         print("--Avg Radius = ", curve_radius, " m")
     
