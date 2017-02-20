daa56ca1fbc040f43e901dc96dfb1b6ac48d58b2
Bryan Hampton <bryan@zeropointfrontiers.com>
2017-02-20 14:57:47 -0600

    Adjusting top-down starting point to be closer to car hood.

--- UtilMask.py
+++ UtilMask.py
@@ -68,11 +68,11 @@ class Perspective:
         
         # staring points
         tops1 = [[550, 450], [ 730, 450] ] # top    left,right
-        bots1 = [[ 50, 646], [1230, 646] ] # bottom left,right
+        bots1 = [[  0, 680], [1230, 680] ] # bottom left,right
         
         # new point location in top-down image
-        tops2 = [[300, 0],      [980, 0]      ] # top    left,right
-        bots2 = [[300, height], [980, height] ] # bottom left,right
+        tops2 = [[275, 0],      [1005, 0]      ] # top    left,right
+        bots2 = [[275, height], [1005, height] ] # bottom left,right
         
         # define transform matrix
         source = np.float32([tops1[0], tops1[1], bots1[1], bots1[0]]) # circular order
